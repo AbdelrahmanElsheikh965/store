09a73489054aa5e88293da101d98d2bcb1ac753f
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-01-27 17:22:28 +0200

    Reactive Form Validation 1st_way
    Form Group (Normal way)

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  RegisterForm : FormGroup;
+
+  constructor(){
+    this.RegisterForm = new FormGroup({
+      fName: new FormControl('', Validators.required),
+      lName: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$")
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      address: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+    });
+  }
+
+  storeProduct(){
+    console.log(this.RegisterForm)
+    console.log(this.RegisterForm.value)    // object to be sent to backend.
+  }
 }
