55c974a339af7f0b667cf71a4211085d04c0a651
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-01-27 15:44:05 +0200

    login template-driven validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MessagesModule } from 'primeng/messages';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [FormsModule, RouterLink, RouterLinkActive, MessagesModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  email: string = '';
+  password: string = '';
+
+  // For debugging
+  checkLogin(){
+    console.log(this.email);
+  }
+
+  handleLogin(form: any){
+    console.log(form.value);
+  }
 }
